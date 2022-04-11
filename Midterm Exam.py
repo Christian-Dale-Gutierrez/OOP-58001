cafb914356eb74cc7130f11cff71cb04f6d008a7
Christian Dale A Gutierrez <99223794+Christian-Dale-Gutierrez@users.noreply.github.com>
2022-04-11 11:40:37 +0800

    Add files via upload

--- Midterm Exam.py
+++ Midterm Exam.py
@@ -1,26 +1,25 @@
 from tkinter import *
 from tkinter import ttk
 window = Tk()
+
+def fullname():
+    txtfld['text'] = 'fullname'
+
+
 window.geometry("500x250+10+20")
 window.title("Midterm in OOP")
 
-str1 = StringVar()
-str2 = StringVar()
-
-def fullname():
-    prnt = (str1.get())
-    str2.set(prnt)
 
 label = Label(window, text = "Enter your fullname:", fg = "red")
-label.place(x=70, y=70)
+label.place(x=60, y=70)
 
-txtfld1 = Entry(window, bd = 5)
-txtfld1.place(x=270, y=65)
+button = Button(window, text = "Click to display your Fullname", fg = "red", command=fullname)
+button.place (x=60, y=145)
 
-button = Button(window, text = "Click to display your Fullname", fg = "red", command=lambda:fullname())
-button.place (x=60, y=140)
+txtfld = Entry(window, textvariable = 'fullname', bd = 5)
+txtfld.place(x=270, y=65)
 
-txtfld2 = Entry(window, bd = 5, textvariable=str2)
-txtfld2.place(x=270, y=140)
+txtfld = Entry(window, text = 'fullname1', bd = 5)
+txtfld.place(x=270, y=145)
 
 window.mainloop()
